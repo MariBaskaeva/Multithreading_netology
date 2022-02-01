cde318c0af147865dc7acc44f108a419a8c56289
MariBaskaeva <mariab1@list.ru>
2022-02-01 21:38:51 +0300

    Improved dealership

--- Car_Dealership/src/Main.java
+++ Car_Dealership/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         final Dealership dealership = new Dealership();
+
         for(int i = 0; i < 10; i++){
+            new Thread(null, dealership::receiveAuto, "Поставщик").start();
             new Thread(null, dealership::sellAuto, "Покупатель " + (i + 1)).start();
-            new Thread(null, dealership::receiveAuto, "Поставщик " + (i + 1)).start();
         }
     }
 }
--- Car_Dealership/src/Seller.java
+++ Car_Dealership/src/Seller.java
@@ -5,22 +5,26 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Seller {
     private final int TIMEOUT = 3000;
     private static int num = 0;
-    private Dealership dealership;
-    private static Lock lock = new ReentrantLock();
-    private static Condition condition = lock.newCondition();
+    private final Dealership dealership;
+    private final Lock lock = new ReentrantLock(true);
+    private final Condition condition = lock.newCondition();
 
     public Seller(Dealership dealership){
         this.dealership = dealership;
     }
 
     public void receiveAuto(){
+        try {
             lock.lock();
             dealership.getAutos().add(new Auto());
             System.out.println(Thread.currentThread().getName() + " выпустил 1 авто");
             condition.signal();
+        }finally{
             lock.unlock();
+        }
     }
     public Auto sellAuto(){
+        Auto result = null;
         try{
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " пришел в салон");
@@ -31,11 +35,12 @@ public class Seller {
             Thread.sleep(TIMEOUT);
             num++;
             System.out.println(num + ") " + Thread.currentThread().getName() + " уехал на новеньком авто.");
+            result = dealership.getAutos().remove(0);
         }catch(InterruptedException ex){
             System.out.println(ex.getMessage());
         }finally{
             lock.unlock();
         }
-        return dealership.getAutos().remove(0);
+        return result;
     }
 }
